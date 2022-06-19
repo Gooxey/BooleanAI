9cfd0dc8a357d21dbd16aeb5eeb6a07f33818491
Gooxey <82870356+Gooxey@users.noreply.github.com>
2022-06-19 13:37:46 +0200

    Corrected the version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='BooleanAI',
     packages=find_packages(include=['booleanai']),
-    version='1.0.0',
+    version='0.1.0',
     description='A different approch to create an AI',
     author='Gooxey',
     license='MIT',
